4a2a746aef1911cfa41bf95c035da04a7685f1b8
rcodingGN <TrumpGN@163.com>
2022-12-05 23:06:12 +0800

    第一次提交

---
+++ 02_初识src/App.vue
@@ -0,0 +1,69 @@
+<template>
+<h1>我是根标签</h1>
+<h1>姓名：{{name}}</h1>
+<h1>年龄：{{age}}</h1>
+<h1>性别：{{sex}}</h1>
+<button @click="sayHello">vue3配置的方法</button>
+<br>
+<br>
+<button @click="sayHelloo">vue2配置的方法</button>
+<br>
+<br>
+<button @click="test1">vue2中读取vue3的数据、方法</button>
+<br>
+<br>
+<button @click="test2">vue3中读取vue2的数据、方法</button>
+</template>
+
+<script>
+// import { h } from "vue";
+export default {
+  name: 'App',
+  data() {
+    return {
+        sex:'男'
+    }
+  },
+  methods: {
+    sayHelloo(){
+        alert('欢迎学习')
+    },
+    test1(){
+        console.log(this.name);
+        console.log(this.sex);
+        console.log(this.age);
+        this.sayHello()
+    }
+  },
+//   此处只是测试，不考虑响应式
+  setup() {
+    let name = '张三';
+    let age = 18;
+    function sayHello() {
+        alert(`我叫${name}，我${age}岁，你好啊`);
+    };
+    function test2(){
+        console.log(age);
+        console.log(name);
+        console.log(sayHello);
+        console.log(this.sayHelloo);
+        console.log(this.sex);
+        
+    }
+    // 返回一个对象（常用）
+    return {
+        name,
+        age,
+        sayHello,
+        test2
+    };
+
+    // 返回一个函数（渲染函数）
+    // return ()=>{return h('h1','尚硅谷')}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03_src_ ref函数/App.vue
@@ -0,0 +1,49 @@
+<template>
+<h1>个人信息</h1>
+<h1>姓名：{{name}}</h1>
+<h1>年龄：{{age}}</h1>
+<h1>工作种类：{{job.type}}</h1>
+<h1>薪水：{{job.salary}}</h1>
+<button @click="changeInfo">修改信息</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: 'App',
+
+//   此处只是测试，不考虑响应式
+  setup() {
+    let name = ref('张三');  // refimpl引用对象
+    let age = ref(18);
+    let job = ref({
+        type:'前端工程师',
+        salary:'30K'
+    })
+
+    function changeInfo() {
+        // name.value = '李四'
+        // age.value = 48
+        // console.log(job.value); // proxy对象
+        job.value.type = '后端工程师'
+        job.value.salary = '35K'
+
+        // console.log(name,age);
+    }
+    // 返回一个对象（常用）
+    return {
+        name,
+        age,
+        changeInfo,
+        job
+    };
+
+    // 返回一个函数（渲染函数）
+    // return ()=>{return h('h1','尚硅谷')}
+  }
+}
+</script>
+
+<style>
+
+</style>
